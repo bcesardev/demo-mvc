ec481f154e27a0e4812f19783023cdbb3d972b2c
Bruno Cesar <bcesar.bruno@gmail.com>
2019-03-15 18:21:18 -0300

    Finalizado parte de Funcionario

--- src/main/java/com/cesar/bruno/curso/boot/service/FuncionarioServiceImpl.java
+++ src/main/java/com/cesar/bruno/curso/boot/service/FuncionarioServiceImpl.java
@@ -1,5 +1,7 @@
 package com.cesar.bruno.curso.boot.service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import com.cesar.bruno.curso.boot.dao.FuncionarioDao;
 import com.cesar.bruno.curso.boot.domain.Funcionario;
 
 @Service
+@Transactional(readOnly = true)
 public class FuncionarioServiceImpl implements FuncionarioService {
 
 	@Autowired
@@ -34,15 +37,35 @@ public class FuncionarioServiceImpl implements FuncionarioService {
 	}
 
 	@Override
-	@Transactional(readOnly = true)
 	public Funcionario buscarPorId(Long id) {
 		return dao.findById(id);
 	}
 
 	@Override
-	@Transactional(readOnly = true)
 	public List<Funcionario> buscarTodos() {
 		return dao.findAll();
 	}
 
+	@Override
+	public List<Funcionario> buscarPorNome(String nome) {
+		return dao.findByNome(nome);
+	}
+
+	@Override
+	public List<Funcionario> buscarPorCargo(Long id) {
+		return dao.findByCargoId(id);
+	}
+
+	@Override
+	public List<Funcionario> buscarPorDatas(LocalDate entrada, LocalDate saida) {
+		if (entrada != null && saida != null) {
+			return dao.findByDataEntradaDataSaida(entrada, saida);
+		} else if (entrada != null) {
+			return dao.findByDataEntrada(entrada);
+		} else if (saida != null) {
+			return dao.findByDataSaida(saida);
+		} else {
+			return new ArrayList<>();
+		}
+	}
 }
--- src/main/java/com/cesar/bruno/curso/boot/web/controller/FuncionarioController.java
+++ src/main/java/com/cesar/bruno/curso/boot/web/controller/FuncionarioController.java
@@ -1,8 +1,10 @@
 package com.cesar.bruno.curso.boot.web.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cesar.bruno.curso.boot.domain.Cargo;
@@ -64,6 +67,25 @@ public class FuncionarioController {
 		funcionarioService.excluir(id);
 		attr.addFlashAttribute("success", "Funcionário removido com sucesso.");
 		return "redirect:/funcionarios/listar";
+	}
+
+	@GetMapping("/buscar/nome")
+	public String getPorNome(@RequestParam("nome") String nome, ModelMap model) {
+		model.addAttribute("funcionarios", funcionarioService.buscarPorNome(nome));
+		return "/funcionario/lista";
+	}
+
+	@GetMapping("/buscar/cargo")
+	public String getPorCargo(@RequestParam("id") Long id, ModelMap model) {
+		model.addAttribute("funcionarios", funcionarioService.buscarPorCargo(id));
+		return "/funcionario/lista";
+	}
+
+	@GetMapping("/buscar/data")
+	public String getPorDatas(@RequestParam("entrada") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate entrada,
+			@RequestParam("saida") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate saida, ModelMap model) {
+		model.addAttribute("funcionarios", funcionarioService.buscarPorDatas(entrada, saida));
+		return "/funcionario/lista";
 	}
 
 	@ModelAttribute("cargos")
